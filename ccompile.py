d44841307e0b4f6a20469aeea1584f95b55b6d7a
PySean <smsod18@hotmail.com>
2017-02-06 18:22:42 -0600

    Forgot about what the argument parser actually returns.

--- ccompile.py
+++ ccompile.py
@@ -44,10 +44,10 @@ if __name__ == '__main__':
                               ' is general to all student files.'
                              ), required=True)
     """
-    parser.add_argument('-h', '--headers', type=str, nargs='*',
+    parser.add_argument('--headers', type=str, nargs='*',
                         help='The header files required for compilation.',
                         required=False)
-    parser.add_argument('-o', 'other_files', type=str, nargs='*',
+    parser.add_argument('-o', '--other_files', type=str, nargs='*',
                         help=('A list of c program files that are required for'
                               ' compilation (i.e., driver program files.'),
                         required=False)
@@ -59,27 +59,28 @@ if __name__ == '__main__':
               " compile/cs3723p1Driver.o  compile/hashApi.o  compile/hexDump64.o {}"
               )
     """
-    parser.parse_args()
+    args = parser.parse_args()
 
-    cmdline = ('gcc -o {outdir}/{name} {other_files}')
+    cmdline = ('gcc -o {outdir}/{name} {other_files}{c_files}')
     #Full pathname to the required header file. Necessary for creating
     #a symbolic link to it within the student's source code directory.
     #header = "/home/sean/classes/recitation/prgms/assign1/cs3723p1.h"
     #headers = ' '.join(parser.headers)
-    for dirpath, dirnames, filenames in os.walk(parser.root):
-        code_dir = any(map(lambda x: x.endswith('.c'), filenames))
+    for dirpath, dirnames, filenames in os.walk(args.root):
+        code_dir = map(lambda x: x.endswith('.c'), filenames)
         headers = [x for x in filenames if x.endswith('.h')]
-        #no_header = any(map(lambda x: x.endswith('.h'), filenames))
         #Is there source code within this directory?
-        if code_dir:
+        if len(code_dir) != 0:
             #Link appropriate header files spec'd on cmdline to compilation
             #directory.
             if len(headers) == 0:
-                map(lambda x: os.link(x, os.path.join(dirpath, x), parser.headers)
+                map(lambda x: os.link(x, os.path.join(dirpath, x), args.headers))
             #TODO: Even needed?
             error = ""
-            cmd_cpy = cmdline.format(outdir=dirpath, name=parser.name, 
-                                     other_files=parser.other_files)
+            #Collapse c source code files into string then format it all up.
+            c_files = ' '.join(code_dir)
+            cmd_cpy = cmdline.format(outdir=dirpath, name=args.name, 
+                                     other_files=args.other_files, c_files=c_files)
             try:
                 error = subprocess.check_output(cmd_cpy.split(), stderr=STDOUT)
             except subprocess.CalledProcessError as cpe:
