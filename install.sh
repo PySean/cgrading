5e2a69f4fa9c83cfe9be3f78b63a710f547302a6
PySean <smsod18@hotmail.com>
2017-03-04 12:30:10 -0600

    Added installation script

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+ln ccompile.py ~/bin/ccompile;
+ln convall2unix.pl ~/bin/convall2unix;
+ln differ.py ~/bin/differ;
+ln genout.py ~/bin/genout;
+ln nospaces.pl ~/bin/nospaces;
+ln unzipper.pl ~/bin/unzipper;
+echo "Done installing!";
